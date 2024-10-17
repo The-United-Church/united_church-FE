da7947bedf4bf847d7cba54c9dedb213d433175e
Samuel Dibiah <samuelchimdidibiah@gmail.com>
2024-10-17 10:05:47 +0100

    created the header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './../styles/nav.css'; 
 
-function Header() {
+const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <nav id="desktop-nav">
+    <div className="logo">The United Church</div>
+    <ul className="nav-links">
+      <li><a href="#events">Events</a></li>
+      <li><a href="#followUps">Follow Ups</a></li>
+      <li><a href="#calender">Calender</a></li>
+      <li><a href="#appointments">Appointments</a></li>
+    </ul>
+  </nav>
+);
+};
 
 export default Header
